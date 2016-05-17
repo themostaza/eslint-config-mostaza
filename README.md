2aa58511aab7a36b5dd59d400863ef3bf089c482
matteo <matteo@themostaza.com>
2016-05-17 16:02:33 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# eslint-config-mostaza
+
+Our eslint shareable config for Node, React and React-Native
+
+`npm i -S eslint eslint-config-mostaza eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react`
---
+++ rules.js
@@ -0,0 +1,33 @@
+module.exports = {
+  "extends": "airbnb",
+  "globals": {
+    "fetch": true,
+    "__DEV__": true
+  },
+  "plugins": ["import"],
+  "rules": {
+    // Standard rules
+    "arrow-parens": 0,
+    "arrow-body-style": 0,
+    "comma-dangle": [2, "never"],
+    "max-len": 0,
+    "new-cap": 0,
+    "no-console": 0,
+    "no-constant-condition": 0,
+    "no-confusing-arrow": 0,
+    "no-else-return": 0,
+    "no-param-reassign": 1,
+    "no-use-before-define": 0,
+    "no-underscore-dangle": 0,
+    "no-unused-vars": [2, { "vars": "all", "args": "none" }],
+    "semi": [2, "never"],
+    "space-before-function-paren": [2, "always"],
+
+    // React rules
+    "react/jsx-boolean-value": 0,
+    "react/prefer-stateless-function": 1,
+
+    // Import rules
+    "import/no-unresolved": [2, { "ignore": ["\.png$"] }]
+  }
+}
