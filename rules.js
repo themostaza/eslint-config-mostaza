849f81a29f10c8e60ee4f08f794a84ee581fc6c8
matteo <matteo@themostaza.com>
2016-05-25 11:09:39 +0200

    No unused expressions + bump to 1.0.2

--- rules.js
+++ rules.js
@@ -19,6 +19,7 @@ module.exports = {
     "no-param-reassign": 1,
     "no-use-before-define": 0,
     "no-underscore-dangle": 0,
+    "no-unused-expressions": 1,
     "no-unused-vars": [1, { "vars": "all", "args": "none" }],
     "semi": [2, "never"],
     "space-before-function-paren": [2, "always"],
